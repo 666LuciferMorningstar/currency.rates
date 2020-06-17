ab4fd6e90cebd57b6bf3d475ea95e7e5f8270e81
Artur <encaps.t@gmail.com>
2020-06-17 14:13:12 +0200

    Initial Commit, layout ready, started making chart.js graph

---
+++ docs/precache-manifest.d316170af388713b73d36781262d00a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abe34266b4cfada4fc2595f95be3209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1189a784f509995c80e6",
+    "url": "/static/css/main.c7768fdd.chunk.css"
+  },
+  {
+    "revision": "40391e1dce1f56f44899",
+    "url": "/static/js/2.53a01607.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.53a01607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1189a784f509995c80e6",
+    "url": "/static/js/main.265afd20.chunk.js"
+  },
+  {
+    "revision": "aaf31c4404ca572fbdea",
+    "url": "/static/js/runtime-main.2c77b172.js"
+  },
+  {
+    "revision": "9991bc69b13afa5aa64a46180c648539",
+    "url": "/static/media/financial5.9991bc69.png"
+  },
+  {
+    "revision": "f9fda927d65b128537c7cd61eb34ad72",
+    "url": "/static/media/shanghai.f9fda927.jpg"
+  }
+]);
